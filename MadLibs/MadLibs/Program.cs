36caeae8b44fd0a026e02ecd07c9210c265c1955
SH-NicholasPage <npage@southhills.edu>
2022-09-03 23:37:25 -0400

    Initial commit

---
+++ MadLibs/MadLibs/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Name: [YOUR NAME HERE]
+* South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
+*/
+
+using System;
+
+namespace MadLibs
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //TODO: Your code here
+        }
+    }
+}
