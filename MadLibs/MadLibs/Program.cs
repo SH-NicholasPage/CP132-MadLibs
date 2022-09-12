4b4cf81f5d5c09b7203f6c954dade7695b7eeb31
SH-NicholasPage <npage@southhills.edu>
2022-09-11 21:45:54 -0400

    Added sample code to help

--- MadLibs/MadLibs/Program.cs
+++ MadLibs/MadLibs/Program.cs
@@ -2,7 +2,7 @@
 * Name: [YOUR NAME HERE]
 * South Hills Username: [YOUR SOUTH HILLS USERNAME HERE]
 */
-
+#pragma warning disable CS8600
 using System;
 
 namespace MadLibs
@@ -11,6 +11,12 @@ namespace MadLibs
     {
         public static void Main()
         {
+            Console.Write("Input a word: ");
+            String input = Console.ReadLine();
+            Console.WriteLine("Your word was " + input);
+            //You should delete the above lines before submitting.
+            //    They are only here to give you an example of how to input and output to the console.
+
             //TODO: Your code here
         }
     }
